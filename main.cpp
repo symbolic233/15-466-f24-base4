1807ae96360a2cdbe567e31f3ea5807a6786e48c
symbolic233 <ymak@andrew.cmu.edu>
2024-09-26 14:22:34 -0400

    Title and screenshot fix

--- main.cpp
+++ main.cpp
@@ -66,7 +66,7 @@ int main(int argc, char **argv) {
 
 	//create window:
 	SDL_Window *window = SDL_CreateWindow(
-		"gp23 game4: choice-based game", //TODO: remember to set a title for your game!
+		"gp23 game4: while Loop Tutorial",
 		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
 		960, 540, //TODO: modify window size if you'd like
 		SDL_WINDOW_OPENGL
